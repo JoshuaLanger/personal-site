8aed303f58b9b290a64ae0f27d5a6a55bc07f048
Joshua Langer <joshuarllanger@gmail.com>
2019-01-25 10:46:06 -0600

    building about page

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,28 +1,31 @@
-var pageList = document.querySelectorAll(".page");
+var pageList = document.querySelectorAll('.page');
 var closeTabArr = [];
 var openTabArr = [];
 
 for (var i = 1; i < pageList.length; i++) {
-  var closeTab = pageList[i].querySelector("#close");
+  var closeTab = pageList[i].querySelector('#close');
   closeTabArr.push(closeTab);
-  var openTab = pageList[i].querySelector("#open");
+  var openTab = pageList[i].querySelector('#open');
   openTabArr.push(openTab);
 }
 
+console.log(openTabArr)
+
 closeTabArr.forEach(tab => {
-  tab.addEventListener("click", function() {
+  tab.addEventListener('click', function() {
     var page = tab.parentElement;
-    tab.style.cursor = "auto"; // TODO: reset all closeTabs back to pointer
-    page.style.zIndex = "10";
-    page.classList.add("close");
+    tab.style.cursor = 'auto'; // TODO: reset all closeTabs back to pointer
+    page.style.zIndex = '10';
+    page.classList.add('close');
   });
 });
 
 openTabArr.forEach(tab => {
-  tab.addEventListener("click", function() {
+  tab.addEventListener('click', function() {
     var page = tab.parentElement;
-    tab.style.cursor = "pointer";
-    page.style.zIndex = "1";
-    page.classList.remove("close");
+    tab.style.cursor = 'pointer';
+    tab.style.zIndex = '-1';
+    page.style.zIndex = '1';
+    page.classList.remove('close');
   });
 });
